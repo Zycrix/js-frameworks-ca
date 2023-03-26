9f74f29c7f72b080e4d2d52cbe0cc99427de5d42
Zycrix <Doffa98@live.no>
2023-03-26 19:46:50 +0200

    Removing some unused vars

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -1,14 +1,12 @@
-import React, { useContext } from "react";
-import { CartContext } from "../../components/reducer";
+import React from "react";
 import useApi from "../../components/hooks/useApi";
 import baseUrl from "../../components/base-url";
 import Loader from "../../components/loader";
 import Product from "../../components/specific";
 
 function App() {
-  const { dispatch } = useContext(CartContext);
   const id = window.location.pathname.split("/")[2];
-  const { data, loading, error } = useApi(`${baseUrl}/${id}`);
+  const { data, loading } = useApi(`${baseUrl}/${id}`);
 
   return <main>{loading === true ? <Loader /> : <Product data={data} />}</main>;
 }
